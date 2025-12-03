9788aa7ea559fb3fd3e417950962f9ea8dee8b9c
Andrew Gunkel <65040985+andrewgunkel@users.noreply.github.com>
2025-12-03 07:40:50 +0100

    FIX sum and total calculated, but additional operations not working yet

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const btnPlus = document.querySelector("#btn-plus");
 const btnMinus = document.querySelector("#btn-minus");
+const btnMultiply = document.querySelector("#btn-multiply");
+const btnDivide = document.querySelector("#btn-divide");
 const btnEquals = document.querySelector("#btn-equals");
 
+const btnZero = document.querySelector("#btn-zero");
 const btnOne = document.querySelector("#btn-one");
 const btnTwo = document.querySelector("#btn-two");
+const btnThree = document.querySelector("#btn-three");
+const btnFour = document.querySelector("#btn-four");
+const btnFive = document.querySelector("#btn-five");
+const btnSix = document.querySelector("#btn-six");
+const btnSeven = document.querySelector("#btn-seven");
+const btnEight = document.querySelector("#btn-eight");
+const btnNine = document.querySelector("#btn-nine");
 
 
 
@@ -14,10 +24,10 @@ console.log(operator);
 
 
 let number1 = "default";
-
 let operator = "";
-
 let number2 = "";
+let total = "";
+let result = "";
 
 
 btnPlus.addEventListener("click",function enterPlus () {
@@ -32,7 +42,20 @@ btnMinus.addEventListener("click",function enterMinus () {
     //operate();
 });
 
-btnOne.addEventListener("click",function enterOne () {
+btnMultiply.addEventListener("click",function enterMultiply () {
+    console.log("x")
+    operator = "*";
+    //operate();
+});
+
+btnDivide.addEventListener("click",function enterDivide () {
+    console.log("÷")
+    operator = "/";
+    //operate();
+});
+
+
+/* btnOne.addEventListener("click",function enterOne () {
     
     if (number1 === "default"){
         number1 = "1"
@@ -40,20 +63,38 @@ btnOne.addEventListener("click",function enterOne () {
     } else {
     number2 = "1";
     console.log("number2 = 1")
-    //operate();
     }
 });
 
 btnTwo.addEventListener("click",function enterTwo () {
-    //console.log("+")
     if (number1 === "default"){
         number1 = "2"
         console.log("number1 = 2")
     } else {
     number2 = "2";
     console.log("number2 = 2")
-    //operate();
     }
+}); */
+
+const numButtons = document.querySelectorAll(".num-btn");
+
+numButtons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const value = btn.dataset.num;
+    console.log("Pressed:", value);
+
+    if (number1 === "default"){
+        //number1 = "2"
+        number1 = value;
+        console.log("number1 is " + number1)
+    } else {
+    number2 = value;
+    console.log("number2 is " + number2)
+    }
+
+
+
+  });
 });
 
 btnEquals.addEventListener("click",operate);
@@ -68,16 +109,27 @@ function operate (){
 
     const a = Number(number1);
 const b = Number(number2);
+
 let result = "";
 
 if (operator === "+") {
-    result =  a + b;
+    result = (total + (a + b));
 } else if (operator === "-") {
-    result = a - b;
+    result = (total + (a - b));
 }
 
-console.log(result)
+total = result; 
+console.log("Total is " + total)
+
+
+number1 = "default";
+number2 = "default";
+
+
+
 } 
+
+
 
 
 //btnMinus.addEventListener("click", (operator = "-"))
